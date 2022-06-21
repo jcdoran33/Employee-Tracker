45e10ced33333932894bc44dc833510c5bc5065e
Jack Doran <jcdoran33@gmail.com>
2022-06-20 21:29:41 -0400

    added requirements for msql and xpress, added port num and app definition

---
+++ server.js
@@ -0,0 +1,10 @@
+// requirements
+const express = require("express");
+const mysql = require("mysql2");
+
+//PORT
+const PORT = process.env.PORT || 3001;
+//define app as express()
+const app = express();
+
+//Express - middleware
