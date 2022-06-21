523994a7bf435bce1aee3ca660a09ea3c60ca691
Jack Doran <jcdoran33@gmail.com>
2022-06-20 22:41:32 -0400

    added psuedo code outline for the main inquirer prompt

--- server.js
+++ server.js
@@ -23,10 +23,10 @@ const db = mysql.createConnection(
         host: "127.0.0.1",
         user: "root",
         password: "password",
-        database: "name_here"
+        database: "name_here" //need to come back and change this once db is created
     },
     console.log("Connected to the database")
-    )
+    );
 
 
 //inquirer launch here (wrap it in a function?)
@@ -40,8 +40,33 @@ function launchInquirer() {
                 choices: ["View All Departments", "View All Roles", "View All Employees", "Add a Department", "Add a Role", "Add an Employee", "Update an Employee Role"]
             }
         ])
+        //think about converting the below IF conditions to a switch statement
         .then((response) => {
-            
+            if (response.selectTask === "View All Departments") {
+                //some code here that runs a query that shows the desired table - probably db.query
+                //then relaunch the prompt from start - launchInquirer();
+            } else if (response.selectTask === "View All Roles") {
+                //some code here that queries for viewing roles
+                //then relaunch the prompt from start - launchInquirer();
+            } else if (response.selectTask === "View All Employees") {
+                //some code here that queries for All EMployees
+                //then relaunch the prompt from start - launchInquirer();
+            } else if (response.selectTask === "Add a Department") {
+                //another inquirer prompt question asking for the name of department, then add that dept to the table
+                //then relaunch the prompt from start - launchInquirer();
+            } else if (response.selectTask === "Add a Role") {
+                //launch another inquirer function that asks for the name, salary, and department for the role and that role is added to the database
+                //then relaunch the prompt from start - launchInquirer();
+            } else if (response.selectTask === "Add an Employee") {
+                //launch a different inquirer function that asks for the employee’s first name, last name, role, and manager, and that employee is added to the database
+                //then relaunch the prompt from start - launchInquirer();
+            } else if (response.selectTask === "Update an Employee Role") {
+                //launch a inquirer function that prompts us to select an employee to update, then asks for their new role (another selection list), then updates the database
+                //then relaunch the prompt from start - launchInquirer();
+            } else {
+                console.log("Error (launchInquirer func): There was an issue with your selection, please select which task you would like to do again.");
+                launchInquirer();
+            }
         })
 
 
