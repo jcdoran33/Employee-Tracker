e0c8475a835c413abf192bae00f753895b5fd689
Jack Doran <jcdoran33@gmail.com>
2022-06-23 18:05:15 -0400

    additional clarifying comments in index.js

--- db/index.js
+++ db/index.js
@@ -4,18 +4,20 @@ class DB {
     constructor(connection) {
         this.connection = connection;
     };
-
+    // query to find all departments
     findAllDepartments () {
         return this.connection.promise().query(`SELECT * FROM department;`)
     };
-
+    //query to find all employee's id, firrst anme, last name, (for use in selecting a manager form drop down list.)
     findAllEmp () {
         return this.connection.promise().query(`SELECT id, first_name, last_name FROM employee`)
     };
-
+    //query to insert the new employee into the employee tables
     addNewEmp (newEmp) {
         return this.connection.promise().query(`INSERT INTO employee SET ?`, newEmp)
     };
+
+    
 }
 
 
