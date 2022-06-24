5c2a669c1bb69c4808c1ca9ab2b3f95072501b7b
Jack Doran <jcdoran33@gmail.com>
2022-06-24 15:27:38 -0400

    fixed sql syntax error in viewAllEmployees method

--- db/index.js
+++ db/index.js
@@ -14,7 +14,7 @@ class DB {
     };
     //query to view all employees - double left join - employee id, first name, lst name, job title, department, salary, manager
     viewAllEmployees () {
-        return this.connection.promise().query(`SELECT id, first_name, last_name, role.title, role.salary, department.name, manager_id FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN employee.manager_id = employee.id`);
+        return this.connection.promise().query(`SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name, employee.manager_id FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON department.id = role.department_id LEFT JOIN employee AS test ON employee.manager_id = employee.id`);
     };
     //query to find all employee's id, firrst anme, last name, (for use in selecting a manager form drop down list.)
     findAllEmp () {
