9ee2de5dfa91b05e6360df72703011aabcd8bced
Jack Doran <jcdoran33@gmail.com>
2022-06-26 11:17:50 -0400

    minor tweaks to queries and aliases

--- db/index.js
+++ db/index.js
@@ -6,7 +6,7 @@ class DB {
     };
     // query to find all departments
     viewAllDepartments () {
-        return this.connection.promise().query(`SELECT * FROM department;`);
+        return this.connection.promise().query(`SELECT id, name AS department_name FROM department;`);
     };
     //query to view all roles
     viewAllRoles () {
@@ -19,7 +19,7 @@ class DB {
     }
     //query to view all employees - double left join - employee id, first name, lst name, job title, department, salary, manager
     viewAllEmployees () {
-        return this.connection.promise().query(`SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name, employee.manager_id FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON department.id = role.department_id LEFT JOIN employee AS test ON employee.manager_id = employee.id`);
+        return this.connection.promise().query(`SELECT employee.id, employee.first_name, employee.last_name, role.title, role.salary, department.name AS department_name, employee.manager_id FROM employee LEFT JOIN role ON employee.role_id = role.id LEFT JOIN department ON department.id = role.department_id LEFT JOIN employee AS test ON employee.manager_id = employee.id`);
     };
     //SQL command to add a new Department into the department table
     addNewDept (newDept) {
